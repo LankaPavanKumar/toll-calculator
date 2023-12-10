4aa2d2a4d6ecf4068f0c309bb23583aff05530d6
Lanka pavan kumar <142505822+LankaPavanKumar@users.noreply.github.com>
2023-12-10 17:39:51 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Calculator from '../components/Calculator';
+import Result from '../components/Result';
+
+const Home = () => {
+  return (
+    <div>
+      <Calculator />
+      <Result />
+    </div>
+  );
+};
+
+export default Home;
